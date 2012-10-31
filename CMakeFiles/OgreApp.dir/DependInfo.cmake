7dace26342c2aa5876f0ff65cc5f3be3b62b5fdb
Valentin Klinghammer <valentin@quelltextfabrik.de>
2012-10-31 13:33:23 +0100

    Project set up

---
+++ CMakeFiles/OgreApp.dir/DependInfo.cmake
@@ -0,0 +1,18 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vk/Games/ogre3d/ogre-basic-tutorial6/TutorialApplication.cpp" "/home/vk/Games/ogre3d/ogre-basic-tutorial6/CMakeFiles/OgreApp.dir/TutorialApplication.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "BOOST_ALL_NO_LIB"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
